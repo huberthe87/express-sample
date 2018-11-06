04cee1f2ac7ed9238e505bfbbd026326c5b05e12
He, Junjie <junjhe@ebay.com>
2018-11-06 22:47:51 +0800

    initial commit

---
+++ routes/sample.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET items in all category. */
+router.get('/category/all', function (req, res, next) {
+    res.status(200).type('json').sendfile('json/category_all.json');
+});
+
+/* GET items of men. */
+router.get('/category/men', function (req, res, next) {
+    res.status(200).type('json').sendfile('json/category_men.json');
+});
+
+/* GET items of women. */
+router.get('/category/women', function (req, res, next) {
+    res.status(200).type('json').sendfile('json/category_women.json');
+});
+
+/* GET categories. */
+router.get('/category', function (req, res, next) {
+    res.status(200).type('json').sendfile('json/master_response.json');
+});
+
+module.exports = router;
